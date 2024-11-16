2c9fc48b7a641846bce6ec38ef046d1137af24d8
Kevin Kuryshev <kuryshevk@gmail.com>
2024-11-16 17:53:34 +0100

    Implemented Hash Table Tries

--- test_tries.py
+++ test_tries.py
@@ -1,14 +1,14 @@
-from tries import VarSizeTrie
+from tries import VarSizeTrie, HashTrie
 
 if __name__ == "__main__":
-    test_trie = VarSizeTrie.create_trie(["a\0", "ab\0"])
+    test_trie = HashTrie.create_trie(["a\0", "ab\0"])
     test_trie.insert("ac\0")
     test_trie.insert("bc\0")
     test_trie.insert("d\0")
     print("")
     #test_trie.delete("a\0")
     test_trie.insert("abc\0")
-    test_trie.delete("ab\0")
+    #test_trie.delete("ab\0")
     test_trie.print()
 
     print(test_trie.contains("d\0"))
--- tries.py
+++ tries.py
@@ -5,7 +5,10 @@ from dataclasses import dataclass
 @dataclass
 class AbstractTrie(ABC):
 
-    char: str
+    children: any
+
+    def is_leaf(self):
+        return not bool(self.children)
 
     def contains(self, word: str) -> bool:
         if len(word) == 0: return True
@@ -40,6 +43,7 @@ class AbstractTrie(ABC):
 class VarSizeTrie(AbstractTrie):
 
     children: list[VarSizeTrie]
+    char: str
 
     def is_leaf(self):
         return not bool(self.children)
@@ -77,23 +81,23 @@ class VarSizeTrie(AbstractTrie):
     @staticmethod
     def __create_trie_for_rest(rest_word: str) -> VarSizeTrie:
         if len(rest_word) == 1:
-            return VarSizeTrie(rest_word, [])
+            return VarSizeTrie(char=rest_word, children=[])
         else:
             lower_node = VarSizeTrie.__create_trie_for_rest(rest_word[1:])
-            return VarSizeTrie(rest_word[0], [lower_node])
+            return VarSizeTrie(char=rest_word[0], children=[lower_node])
         
     @staticmethod
     def create_trie(words: str) -> VarSizeTrie:
-        trie = VarSizeTrie("root", [])
-        trie.has_parent = False
+        trie = VarSizeTrie(char="root", children=[])
         for word in words: trie.insert(word)
         return trie
     
-    def print(self, is_root=True, depth=0):
-        prefix = ""
-        for _ in range(depth):
-            prefix += "   "
-        prefix += "└──"
+    def print(self, is_root=True, depth=0, is_last=False, was_last=False):
+        prefix = "   "
+        for _ in range(depth-2):
+            prefix += "│  "
+        if depth > 1: prefix += "│  " if not was_last else "   "
+        prefix += "├──" if not is_last else "└──"
 
         if is_root:
             print(self.char)
@@ -102,4 +106,67 @@ class VarSizeTrie(AbstractTrie):
             print(prefix + char)
 
         for child in self.children:
-            child.print(False, depth+1)
+            child.print(False, depth+1, id(child) == id(self.children[-1]), is_last)
+    
+
+@dataclass
+class HashTrie(AbstractTrie):
+
+    children: dict[str, HashTrie]
+    
+    def _contains(self, word: str) -> bool:
+        if word[0] in self.children:
+            return self.children[word[0]].contains(word[1:])
+        return False
+    
+    def _delete(self, word: str) -> bool:
+        success = False
+        if word[0] in self.children:
+            child = self.children[word[0]]
+            success = child.delete(word[1:])
+        #else: print(f"{word[0]} does not appear in {self.children}")
+        if success:
+            #print(f"For {word[0]} deleting?: {child}")
+            if child.is_leaf():
+            #    print(f"Deleting...")
+                self.children.pop(word[0])
+        return success
+    
+    def _insert(self, word: str) -> bool:
+        success = True
+        if word[0] in self.children:
+            success = self.children[word[0]].insert(word[1:])
+        else:
+            new_trie = self.__create_trie_for_rest(word[1:])
+            self.children[word[0]] = new_trie
+        return success
+    
+    @staticmethod
+    def __create_trie_for_rest(rest_word: str) -> HashTrie:
+        if len(rest_word) == 1:
+            return HashTrie({rest_word: HashTrie({})})
+        else:
+            lower_node = HashTrie.__create_trie_for_rest(rest_word[1:])
+            return HashTrie({rest_word[0]: lower_node})
+        
+    @staticmethod
+    def create_trie(words: str) -> HashTrie:
+        trie = HashTrie({})
+        for word in words: trie.insert(word)
+        return trie
+    
+    def print(self, char="", is_root=True, depth=0, is_last=False, was_last=False):
+        prefix = "   "
+        for _ in range(depth-2):
+            prefix += "│  "
+        if depth > 1: prefix += "│  " if not was_last else "   "
+        prefix += "├──" if not is_last else "└──"
+
+        if is_root:
+            print("root")
+        else:
+            char = char if char != "\0" else "$"
+            print(prefix + char)
+
+        for child in self.children:
+            self.children[child].print(child, False, depth+1, id(child) == id(list(self.children.keys())[-1]), is_last)
